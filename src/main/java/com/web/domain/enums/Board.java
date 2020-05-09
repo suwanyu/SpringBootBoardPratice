5a464b66ce5ddd1ddca6427ab65d25cd05c7abca
suwanyu <tndhks123@nate.com>
2020-05-09 19:29:49 +0900

    프로젝트 생성

---
+++ src/main/java/com/web/domain/enums/Board.java
@@ -0,0 +1,46 @@
+package com.web.domain.enums;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class Board implements Serializable {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column
+    private String title;
+
+    @Column
+    private String subTitle;
+
+    @Column
+    private String content;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private BoardType boardType;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+
+
+
+}
